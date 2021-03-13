e69fd8045f5d9b6ea57d9260927c6896cd5652c2
root <root@localhost.localdomain>
2021-03-13 10:57:20 +0530

    adding files

---
+++ app.py
@@ -0,0 +1,31 @@
+import subprocess as sp
+from flask import Flask , render_template , request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return  render_template("welcome.html")
+
+@app.route('/home')
+def home():
+    return render_template("lvm.html")
+
+@app.route('/test', methods=["GET"])
+def test():
+    ch=request.args.get("choice")    
+    if ch == '1':
+        return("Your choice : " + ch + " : To view available storage devices")
+    elif ch == '2':
+        return("Your choice : " + ch + " : To view available partitions")
+    elif ch == '3':
+        return("Your choice : " + ch + " : To work on PV (Persistent Volume)")
+    elif ch == '4':
+        return("Your choice : " + ch + " : To work on VG (Volume Group)")
+    elif ch =='5':
+        return("Your choice : " + ch + " : To work on LV (Logical Volume)")
+    else:
+        return("Invalid choice try again!!")
+
+if __name__ == '__main__':
+    app.run()
